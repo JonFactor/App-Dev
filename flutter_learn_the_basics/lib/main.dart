15709512275060b74fb7ad2a7e66fa37b125b700
JonFactor <111780795+JonFactor@users.noreply.github.com>
2023-09-02 23:34:47 -0400

    souther skies

--- flutter_learn_the_basics/lib/main.dart
+++ flutter_learn_the_basics/lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String buttonName = 'Click';
+  int bottomBarIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -14,18 +22,32 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text("I luv tutorials"),
         ),
-        body: const Text("im in the body now"),
+        body: Center(
+          child: ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  buttonName = 'dododo $buttonName';
+                });
+              },
+              child: Text(buttonName)),
+        ),
         bottomNavigationBar: BottomNavigationBar(
           items: const [
-             BottomNavigationBarItem(
+            BottomNavigationBarItem(
               label: "Home",
-              icon: Icon(Icons.home)
+              icon: Icon(Icons.home),
             ),
-             BottomNavigationBarItem(
-              label:"Home",
-              icon: Icon(Icons.home)
+            BottomNavigationBarItem(
+              label: "Settings",
+              icon: Icon(Icons.settings),
             )
           ],
+          currentIndex: bottomBarIndex,
+          onTap: (int index) {
+            setState(() {
+              bottomBarIndex = index;
+            });
+          },
         ),
       ),
 
