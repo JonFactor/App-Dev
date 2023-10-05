68de23b61ce28e35c5a2f36ffa9021ef07670bf2
JonFactor <Jon.Factor2@gmail.com>
2023-10-05 15:13:49 -0400

    group page routing complete

--- promoto/app/(tabs)/groups/index.tsx
+++ promoto/app/(tabs)/groups/index.tsx
@@ -1,15 +1,39 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
+import GroupTypes from "../../components/cards/GroupTypes";
 
 const GroupsPage = () => {
+  const groupTypeList = [
+    ["sports", "football.png", "#F78E78"],
+    ["music", "guitar.png", "#F1F778"],
+    ["video games", "controler.png", "#78F780"],
+    ["chess", "pawn.png", "#78D4F7"],
+    ["exersize", "weights.png", "#A678F7"],
+    ["educational", "glasses.png", "#F778DE"],
+  ];
   return (
-    <View>
-      <Link href="/groups/1">Catigory 1</Link>
-      <Link href="/groups/2">Catigory 2</Link>
-      <Link href="/groups/3">Catigory 3</Link>
+    <View style={styles.groupContainers}>
+      {groupTypeList.map((item, index) => {
+        return (
+          <GroupTypes
+            key={index}
+            color={item[2]}
+            scale={1}
+            title={item[0]}
+            logo={item[1]}
+            gradient={true}
+          />
+        );
+      })}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  groupContainers: {
+    display: "flex",
+  },
+});
 
 export default GroupsPage;
