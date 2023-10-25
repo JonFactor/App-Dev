c0815ff375edaf7d8ee789925254e0c2c483650e
JonFactor <Jon.Factor2@gmail.com>
2023-10-25 07:17:42 -0400

    unstaged

---
+++ promoto/frontend/components/modals/AddUserModal.tsx
@@ -0,0 +1,15 @@
+import { View, Text, ScrollView } from "react-native";
+import React, { useState } from "react";
+import EventRegisterModalTemplate from "./EventRegisterModalTemplate";
+
+const AddUserModal = (setter, parentSetter) => {
+  const [selectedItems, setSelectedItems] = useState([]);
+  const GroupSelection = (value) => {};
+  return (
+    <EventRegisterModalTemplate setter={setter}>
+      <ScrollView className=" p-4"></ScrollView>
+    </EventRegisterModalTemplate>
+  );
+};
+
+export default AddUserModal;
